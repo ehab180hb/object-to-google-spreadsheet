ba81859cfc77cf74248a4bfa06d8709e0db5fafa
Ehab Khaireldin <ehabkhaireldin@gmail.com>
2019-01-26 20:27:17 +0100

    fixing indentation

--- src/index.js
+++ src/index.js
@@ -4,50 +4,50 @@ const { validate } = require('./validation');
 function jsonToGssFormat({ docs, rowName, properties, a1Field, sort }) {
   const firstRowSet = new Set();
   
-	docs.forEach(inpObject => Object.keys(inpObject[properties])
-		.forEach(keyName => firstRowSet.add(keyName)));
+  docs.forEach(inpObject => Object.keys(inpObject[properties])
+    .forEach(keyName => firstRowSet.add(keyName)));
 
-	let header = Array.from(firstRowSet);
-	header = sort ? header.sort() : header;
-	header.unshift(a1Field);
+  let header = Array.from(firstRowSet);
+  header = sort ? header.sort() : header;
+  header.unshift(a1Field);
 
-	const fillData = docs.map(inpObject => {
-		return header.map(uniqueKey => {
-			return uniqueKey == a1Field ? inpObject[rowName]
-				: inpObject[properties][uniqueKey] ? arrSringify(inpObject[properties][uniqueKey])
-					: false;
-		});
-	});
+  const fillData = docs.map(inpObject => {
+    return header.map(uniqueKey => {
+      return uniqueKey == a1Field ? inpObject[rowName]
+        : inpObject[properties][uniqueKey] ? arrSringify(inpObject[properties][uniqueKey])
+          : false;
+    });
+  });
 
-	return [header, ...fillData];
+  return [header, ...fillData];
 }
 
 function arrSringify(input) {
-	return input instanceof Array ? input.join(', ') : input;
+  return input instanceof Array ? input.join(', ') : input;
 }
 
 function cleanChars(input) {
-	return typeof input == 'string' ? input.replace(/[\u0000-\u001f]/g, '') : input;
+  return typeof input == 'string' ? input.replace(/[\u0000-\u001f]/g, '') : input;
 }
 
 async function pushToSheet(auth, data) {
-			const a2gs = new ArrayToGoogleSheets(auth.docKey, auth.creds);
-			const options = { margin: 2, minRow: 10, minCol: 10, resize: true, clear: false };
-			await a2gs.updateGoogleSheets(data.sheetName, data.values, options);
-			return { ok: 1, rowCount: data.values.length };
+  const a2gs = new ArrayToGoogleSheets(auth.docKey, auth.creds);
+  const options = { margin: 2, minRow: 10, minCol: 10, resize: true, clear: false };
+  await a2gs.updateGoogleSheets(data.sheetName, data.values, options);
+  return { ok: 1, rowCount: data.values.length };
 }
 
 
 module.exports = class {
-	/**
+  /**
 	 * Instantiate an instance of the module
 	 * @param {object} creds - your Google's service account credintials.
 	 * @param {string} docKey - the ID of your spreadsheet.
 	 */
-	constructor(creds, docKey) {
-		this.auth = { creds, docKey };
-	}
-	/**
+  constructor(creds, docKey) {
+    this.auth = { creds, docKey };
+  }
+  /**
 	 * Push your data to the sheet
 	 * @param {array} docs - the array of your objects
 	 * @param {object} options 
@@ -59,25 +59,25 @@ module.exports = class {
 	 * @param {boolean} options.sort - sort chronologically.
 	 * @returns {object} - confirmation of completion.
 	 */
-	async push(docs, options = {}) {
+  async push(docs, options = {}) {
     const { auth } = this;
     
-		const validation = validate({ docs, options, auth });
+    const validation = validate({ docs, options, auth });
     if (validation.error) throw new Error(validation.error);
     
-		const {
-			sheetName = 'New Sheet',
-			rowName = Object.keys(docs[0])[0],
-			properties = Object.keys(docs[0])[1],
-			a1Field = ' ',
-			sort,
-			removeBase
+    const {
+      sheetName = 'New Sheet',
+      rowName = Object.keys(docs[0])[0],
+      properties = Object.keys(docs[0])[1],
+      a1Field = ' ',
+      sort,
+      removeBase
     } = options;
     
-		let values = jsonToGssFormat({ docs, rowName, properties, a1Field, sort });
-		values = values.map(x => x.map(y => cleanChars(y)));
+    let values = jsonToGssFormat({ docs, rowName, properties, a1Field, sort });
+    values = values.map(x => x.map(y => cleanChars(y)));
     values = removeBase ? values.map(x => x.slice(1)) : values;
     
-		return pushToSheet(auth, { values, sheetName })
-	}
+    return pushToSheet(auth, { values, sheetName });
+  }
 };
--- src/test/main.js
+++ src/test/main.js
@@ -10,147 +10,147 @@ let badInputObjects;
 
 describe('main module', () => {
 
-	const fakeCreds = {};
-	const fakeDocKey = '1gsLxL0ln8m2yJQgnLNpzvP8kGDLU1ATszwZlM';
+  const fakeCreds = {};
+  const fakeDocKey = '1gsLxL0ln8m2yJQgnLNpzvP8kGDLU1ATszwZlM';
 
-	const goodInputObjects = [
-		{
-			person: 'John',
-			properties: {
-				Age: 25,
-				Address: '16 main st.'
-			}
-		},
-		{
-			person: 'Jane\u0000\u0000',
-			properties: {
-				Age: 26,
-				Hobbies: ['swimming', 'Javascripting']
-			}
+  const goodInputObjects = [
+    {
+      person: 'John',
+      properties: {
+        Age: 25,
+        Address: '16 main st.'
+      }
+    },
+    {
+      person: 'Jane\u0000\u0000',
+      properties: {
+        Age: 26,
+        Hobbies: ['swimming', 'Javascripting']
+      }
 
-		}
-	];
+    }
+  ];
 
-	badInputObjects = [
-		'some string',
-		{
-			person: 'Jane',
-			properties: {
-				Age: 26,
-				Hobbies: ['swimming', 'Javascripting']
-			}
+  badInputObjects = [
+    'some string',
+    {
+      person: 'Jane',
+      properties: {
+        Age: 26,
+        Hobbies: ['swimming', 'Javascripting']
+      }
 
-		}
-	];
+    }
+  ];
 
-	const options = {
-		sheetName: 'My Awesome Report',
-		rowName: 'person',
-		properties: 'properties',
-		a1Field: 'info'
-	};
+  const options = {
+    sheetName: 'My Awesome Report',
+    rowName: 'person',
+    properties: 'properties',
+    a1Field: 'info'
+  };
 
-	const fakePush = (auth, data) => {
-		return {
-			auth,
-			data
-		};
-	};
+  const fakePush = (auth, data) => {
+    return {
+      auth,
+      data
+    };
+  };
 
-	ObjectToGoogleSheet.__set__('pushToSheet', fakePush);
+  ObjectToGoogleSheet.__set__('pushToSheet', fakePush);
 
-	before(async () => {
-		myModule = new ObjectToGoogleSheet(fakeCreds, fakeDocKey);
-		[
-			goodResult,
-			goodResultNoOpts,
-			goodResultWithSort
-		] = await Promise.all(
-			[
-				myModule.push(goodInputObjects, options),
-				myModule.push(goodInputObjects),
-				myModule.push(goodInputObjects, { sort: true, removeBase: true })
-			]
-		);
-	});
+  before(async () => {
+    myModule = new ObjectToGoogleSheet(fakeCreds, fakeDocKey);
+    [
+      goodResult,
+      goodResultNoOpts,
+      goodResultWithSort
+    ] = await Promise.all(
+      [
+        myModule.push(goodInputObjects, options),
+        myModule.push(goodInputObjects),
+        myModule.push(goodInputObjects, { sort: true, removeBase: true })
+      ]
+    );
+  });
 
 
 
-	it('should process valid auth inputs', async () => {
-		expect(goodResult).to.have.property('auth')
-			.which.has.property('creds', fakeCreds);
+  it('should process valid auth inputs', async () => {
+    expect(goodResult).to.have.property('auth')
+      .which.has.property('creds', fakeCreds);
 
-		expect(goodResult).to.have.property('auth')
-			.which.has.property('docKey', fakeDocKey);
-	});
+    expect(goodResult).to.have.property('auth')
+      .which.has.property('docKey', fakeDocKey);
+  });
 
-	it('should properly map the head', () => {
-		expect(goodResult).to.have.property('data')
-			.which.has.property('values')
-			.that.is.an.instanceOf(Array)
-			.with.lengthOf(3)
-			.that.has.property('0')
-			.which.has.property('3')
-			.that.equals('Hobbies');
-	});
+  it('should properly map the head', () => {
+    expect(goodResult).to.have.property('data')
+      .which.has.property('values')
+      .that.is.an.instanceOf(Array)
+      .with.lengthOf(3)
+      .that.has.property('0')
+      .which.has.property('3')
+      .that.equals('Hobbies');
+  });
 
-	it('should sort and remove base', () => {
-		expect(goodResultWithSort).to.have.property('data')
-			.which.has.property('values')
-			.that.is.an.instanceOf(Array)
-			.with.lengthOf(3)
-			.that.has.property('0')
-			.which.has.property('2')
-			.that.equals('Hobbies');
-	});
+  it('should sort and remove base', () => {
+    expect(goodResultWithSort).to.have.property('data')
+      .which.has.property('values')
+      .that.is.an.instanceOf(Array)
+      .with.lengthOf(3)
+      .that.has.property('0')
+      .which.has.property('2')
+      .that.equals('Hobbies');
+  });
 
-	it('should properly map the body', () => {
-		expect(goodResult).to.have.property('data')
-			.which.has.property('values')
-			.that.is.an.instanceOf(Array)
-			.with.lengthOf(3)
-			.that.has.property('1')
-			.which.has.property('1')
-			.that.equals(25);
-	});
+  it('should properly map the body', () => {
+    expect(goodResult).to.have.property('data')
+      .which.has.property('values')
+      .that.is.an.instanceOf(Array)
+      .with.lengthOf(3)
+      .that.has.property('1')
+      .which.has.property('1')
+      .that.equals(25);
+  });
 
-	it('should figure out missing options', () => {
-		expect(goodResultNoOpts).to.have.property('data')
-			.which.has.property('values')
-			.that.is.an.instanceOf(Array)
-			.with.lengthOf(3)
-			.that.has.property('1')
-			.which.has.property('1')
-			.that.equals(25);
-	});
+  it('should figure out missing options', () => {
+    expect(goodResultNoOpts).to.have.property('data')
+      .which.has.property('values')
+      .that.is.an.instanceOf(Array)
+      .with.lengthOf(3)
+      .that.has.property('1')
+      .which.has.property('1')
+      .that.equals(25);
+  });
 
-	it('should properly map array properties', () => {
-		expect(goodResult).to.have.property('data')
-			.which.has.property('values')
-			.that.is.an.instanceOf(Array)
-			.with.lengthOf(3)
-			.that.has.property('2')
-			.which.has.property('3')
-			.that.equals('swimming, Javascripting');
-	});
+  it('should properly map array properties', () => {
+    expect(goodResult).to.have.property('data')
+      .which.has.property('values')
+      .that.is.an.instanceOf(Array)
+      .with.lengthOf(3)
+      .that.has.property('2')
+      .which.has.property('3')
+      .that.equals('swimming, Javascripting');
+  });
 
-	it('should clean input out of invalid characters', () => {
-		expect(goodResult).to.have.property('data')
-			.which.has.property('values')
-			.that.is.an.instanceOf(Array)
-			.with.lengthOf(3)
-			.that.has.property('2')
-			.which.has.property('0')
-			.that.has.lengthOf(4);
-	});
+  it('should clean input out of invalid characters', () => {
+    expect(goodResult).to.have.property('data')
+      .which.has.property('values')
+      .that.is.an.instanceOf(Array)
+      .with.lengthOf(3)
+      .that.has.property('2')
+      .which.has.property('0')
+      .that.has.lengthOf(4);
+  });
 
-	it('should not process invalid input', () => {
-		return myModule.push(badInputObjects, options)
-			.catch((error) => {
-				expect(error).to.be.instanceof(Error)
-					.and.to.match(/must be an object/);
-			});
+  it('should not process invalid input', () => {
+    return myModule.push(badInputObjects, options)
+      .catch((error) => {
+        expect(error).to.be.instanceof(Error)
+          .and.to.match(/must be an object/);
+      });
 
-	});
+  });
 
 });
